5378dd2274cc26c703a8c0255b246accae570970
mhrobles <mhrobles@uc.cl>
2022-06-26 00:55:04 -0400

    avance

--- Sites/index.php
+++ Sites/index.php
@@ -16,6 +16,9 @@ if ($request_method == 'POST') {
 
     $data = $result -> fetchAll();
 
+    $_SESSION['user_id'] = $data;
+    $_SESSION['user_name'] = $password;
+
     if (!empty($data[0][0])) { 
         go_inicio();
     } else {
