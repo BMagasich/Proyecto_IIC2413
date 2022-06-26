aa46fa8978ea176d082c498f71bb3645536e9419
mhrobles <mhrobles@uc.cl>
2022-06-25 21:13:53 -0400

    segundo intento

--- Sites/views/__init__.php
+++ Sites/views/__init__.php
@@ -7,7 +7,7 @@ require_once __DIR__ . "/../config/conection.php";
 session_start();
 
 function go_home() {
-    header("Location: https://codd.ing.puc.cl/~grupo25/index.php");
+    header("Location: /../index.php");
     exit();
 }
 
