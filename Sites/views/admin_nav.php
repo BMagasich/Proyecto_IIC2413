7ee1d98264c3166004790cc3dd5620c47a9535bb
mhrobles <mhrobles@uc.cl>
2022-06-26 14:55:58 -0400

    arreglo path

--- Sites/views/admin_nav.php
+++ Sites/views/admin_nav.php
@@ -1,5 +1,5 @@
 <?php 
-require 'views/__init__.php';
+require '__init__.php';
 $request_method = filter_input(INPUT_SERVER, 'REQUEST_METHOD');
 
 if ($request_method == 'POST') {
@@ -31,8 +31,7 @@ include('../templates/header.html'); ?>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="index.php">Inicio</a></li>
-                        <li class="nav-item"><a class="nav-link" href="tabla.php">Consultas</a></li>
+                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="../index.php">Cerrar Sesion</a></li>
                     </ul>
                 </div>
             </div>
