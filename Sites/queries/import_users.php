ddd76b80f92103cba2ced7b62b2244ac245f3c65
mhrobles <mhrobles@uc.cl>
2022-06-25 21:18:03 -0400

    cuarto intento

--- Sites/queries/import_users.php
+++ Sites/queries/import_users.php
@@ -1,6 +1,3 @@
-<?php include('../templates/header.html'); ?>
-
-<body>
 <?php
         require("../config/conection.php");
 
@@ -15,5 +12,3 @@
         require_once '__init__.php';
         go_home();
     ?>
-
-<?php include('../templates/footer.html'); ?>
