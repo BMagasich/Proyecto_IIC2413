c03ffe1c244162a9c3963caca5a27dd9bdd7276c
JUAN JOHNSON <juan.johnson@uc.cl>
2022-06-28 21:04:58 -0400

    arreglo ruta

--- Sites/views/admin_nav.php
+++ Sites/views/admin_nav.php
@@ -67,7 +67,7 @@ include('../templates/header.html'); ?>
         <!-- Responsive navbar-->
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
             <div class="container px-5">
-                <a class="navbar-brand" href="user_nav.php">Proyecto Grupo 25 & 92: Bienvenido <?php echo $_SESSION["username"]?></a>
+                <a class="navbar-brand" href="admin_nav.php">Proyecto Grupo 25 & 92: Bienvenido <?php echo $_SESSION["username"]?></a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
